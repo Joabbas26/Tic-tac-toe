d7841642a712939fe7d8dbe20d95f03d702f28e1
Joab Bastidas <Joabbastidas@gmail.com>
2016-08-06 17:26:07 -0500

    Replace default code
    Added code for the game board

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,10 +1,27 @@
-#include <QCoreApplication>
+//Project: Tic-tac-toe using mouse input
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
+void Board() {}
+void X_Os() {}
 
-    return a.exec();
+int main() {
+
+    cout << "Hello, World!\n";
+    return 0;
 }
+
+void Board() {
+    char a, b, c, d, e, f, g, h, i;
+    cout << " | " << a << " | " << b << " | " << c << "| "<< endl;
+    cout << " | " << d << " | " << e << " | " << f << "| "<< endl;
+    cout << " | " << g << " | " << h << " | " << i << "| "<< endl;
+
+}
+
+void X_Os() {
+    char piece;
+    cout << "Enter: X or O " << endl;
+
+    }
